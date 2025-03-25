8898b2379bbba91d9085d7eb3be8f3bbf696f8fc
edward-tacohands <eddierecochiropractic@gmail.com>
2025-03-25 18:38:00 +0100

    initial commit

---
+++ data/courses.js
@@ -0,0 +1,47 @@
+export const courses = [
+    {
+        id: 1,
+        title: 'Systemutveckling C#' ,
+        duration: 20,
+        location: 'Malmö',
+        extent: '100%',
+        applyBefore: '31 mars 2026',
+        imageUrl: 'image2.jpg'
+    },
+    {
+        id: 2,
+        title: 'Webbutveckling JavaScript' ,
+        duration: 20,
+        location: 'Luleå',
+        extent: '100%',
+        applyBefore: '31 mars 2026',
+        imageUrl: 'image3.jpg'
+    },
+    {
+        id: 3,
+        title: 'DevOps' ,
+        duration: 20,
+        location: 'Distans',
+        extent: '100%',
+        applyBefore: '31 mars 2026',
+        imageUrl: 'image4.jpg'
+    },
+    {
+        id: 4,
+        title: 'Cloud Developer' ,
+        duration: 20,
+        location: 'Distans',
+        extent: '100%',
+        applyBefore: '31 mars 2026',
+        imageUrl: 'image5.jpg'
+    },
+    {
+        id: 5,
+        title: 'Hovslagare' ,
+        duration: 20,
+        location: 'Malmö',
+        extent: '100%',
+        applyBefore: '31 mars 2026',
+        imageUrl: 'image6.jpg'
+    }
+]
